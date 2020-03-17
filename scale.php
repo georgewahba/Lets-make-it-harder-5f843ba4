db39aea8cd9dbd28bd5ab234d5d711fc41d86491
GeorgeWahba <george-wahba@hotmail.com>
2020-03-17 12:19:33 +0100

    Uitwerking van de exercise

--- scale.php
+++ scale.php
@@ -1,13 +1,12 @@
 <?php
-if ($argv[1]==$argv[2]) {
+if ($argv[1] == $argv[2]) {
     echo "in balans";
-}
-else {
+} else {
     $over = ($argv[1] - $argv[2]);
-	$erbij = explode(',', $argv[3]);
-	foreach ($erbij as $value) {
-		if ($argv[1] + $value == $argv[2] || $argv[2] + $value == $argv[1]) {
+    $erbij = explode(',', $argv[3]);
+    foreach ($erbij as $value) {
+        if ($argv[1] + $value == $argv[2] || $argv[2] + $value == $argv[1]) {
             echo $value;
-        }   
+        }
     }
 }
