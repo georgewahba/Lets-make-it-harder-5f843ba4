547dc0b1ac1bd12c9c5199d2393463d79f4debad
GeorgeWahba <george-wahba@hotmail.com>
2020-03-17 10:56:13 +0100

    Uitwerking van de exercise

--- scale.php
+++ scale.php
@@ -3,5 +3,11 @@ if ($argv[1]==$argv[2]) {
     echo "in balans";
 }
 else {
-    echo "is niet in balans";
+    $over = ($argv[1] - $argv[2]);
+	$erbij = explode(',', $argv[3]);
+	foreach ($erbij as $value) {
+		if ($argv[1] + $value == $argv[2] || $argv[2] + $value == $argv[1]) {
+            echo $value;
+        }   
+    }
 }
